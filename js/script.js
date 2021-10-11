be723a23a9e00d6fa30cb60d0de92b24e4335a8d
azamat khabibullaev <azamatstechblog@gmail.com>
2021-10-11 10:01:38 +0500

    Consulting Project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(function() {
+  $('.slider__inner').slick({
+    nextArrow: `<button type="button" class="slider-btn slick-next"></button>`,
+    prevArrow: `<button type="button" class="slider-btn slick-prev"></button>`,
+    infinite: false
+  });
+})
